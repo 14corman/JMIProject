3598a0c885eba62ea3212c4f54f556e633f23b8d
Andrew <arwest@uiowa.edu>
2019-02-26 16:01:47 -0600

    added mic preprocessing to preprocessor and is working correctly

--- preprocesor.py
+++ preprocesor.py
@@ -12,6 +12,9 @@ import os
 geneStart = 46
 geneEnd = 197
 
+micStart = 19   # 0 indexing
+micEnd = 45
+
 #The path to Required Information for Analysis.xlsx
 reqInformationPath = os.getcwd() + "/Required Information for Analysis.xlsx"
 
@@ -24,14 +27,14 @@ debug = True
 def main():
     """
     Author Cory Kromer-Edwards
-    Edits by: ...
+    Edits by: Andrew West
     The main method to preprocessing. This method will open the dataset csv
     file, and send each row to the preprocessing steps.
     """
     with open('dataset.csv') as csvFile:
         csvReader = csv.reader(csvFile, delimiter=',')
         lineCount = 0
-        (esblNames, carbaNames) = getEsblCarba()
+        (esblNames, carbaNames, micBreakpoints) = getEsblCarba()
         for row in csvReader:
             if lineCount == 0:
                 isolateNames = getIsolate(row)
@@ -44,11 +47,13 @@ def main():
                 if testRow(row):
                     isolate = getGenes(row)
                     convertedEsblCarba = convertEsblCarba(isolateNames, esblNames, carbaNames, isolate)
+                    mics = getMicValues(row, micBreakpoints)
                     
                     if debug:
                         print("\nRow number " + str(lineCount))
                         print("Genes: ", isolate)
                         print("ESBL, Carba: ", convertedEsblCarba)
+                        print("MICs: ", mics)
                         
                     
                 lineCount += 1
@@ -85,7 +90,7 @@ def getIsolate(row):
 def getEsblCarba():
     """
     Author: Cory Kromer-Edwards
-    Edits by: ...
+    Edits by: Andrew West
     Gets the esbl and carba. groups from the required information excel workbook,
     and puts the categories into sets and returns those sets.
     """
@@ -94,17 +99,21 @@ def getEsblCarba():
         
     esbl = set()
     carba = set()
+    mic = []
     wb = xlrd.open_workbook((reqInformationPath)) 
-    sheet = wb.sheet_by_index(2) 
-      
+    sheet_2 = wb.sheet_by_index(2)
+    sheet_3 = wb.sheet_by_index(3)
+
+    for i in range(1, 48):
+        mic.append((sheet_3.cell_value(i, 1), sheet_3.cell_value(i, 2)))
       
     for i in range(1, 424): 
-        esbl.add(sheet.cell_value(i, 0))
+        esbl.add(sheet_2.cell_value(i, 0))
                 
     for i in range(1, 585): 
-        carba.add(sheet.cell_value(i, 1))
+        carba.add(sheet_2.cell_value(i, 1))
                 
-    return (esbl, carba)
+    return (esbl, carba, mic)
   
   
 def convertEsblCarba(isolateNames, esbl, carba, isolate):
@@ -161,7 +170,40 @@ def getGenes(row):
             raise ValueError("Gene is not of type 'neg' or 'pos', but is {row[i]}")
     
     return genes
-        
+
+def getMicValues(row, micBreakpoints):
+    """
+    Author: Andrew West
+    Edits by: ...
+    getMicValues takes in the row from the csv, and extracts the mic values from
+    the row. It then comapres their value value to the breakpoints for that MIC.
+    It returns _ if less than or equal to the lower breakpoint, 2 if in between
+    the breakpoints, and _ if greater than or equal to the upper breakpoint
+    """
+    mic = []
+    for i in range(micStart, micEnd):
+        if row[i] == '':
+            mic.append(0)
+            continue
+        micValue = float(row[i])
+        if micValue <= micBreakpoints[i][0]:
+            mic.append(3)
+        elif micValue >= micBreakpoints[i][1]:
+            mic.append(1)
+        else:
+            mic.append(2)
+    return mic
+
+def getMicNames(row):
+    """
+    Author: Andrew West
+    Edits by: ...
+    Gets the MIC names. It returns a list of Stirngs.
+    """
+    mic = []
+    for i in range(micStart, micEnd):
+        mic.append(row[i])
+    return mic
 
 
 
