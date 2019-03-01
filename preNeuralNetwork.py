d95edf45a37722cf4b869917570bcfcf7002c359
Kromer-Edwards, Cory J <cory-edwards@uiowa.edu>
2019-03-01 11:04:34 -0600

    Updated py files
    Improved debuging output of the neural network.
    Fixed drug issue with MICs since we were looking at 47 drugs when they only use 26.
    Added more detail to preprocessor output, and allowed for a more robust debug feature.

--- preNeuralNetwork.py
+++ preNeuralNetwork.py
@@ -9,12 +9,14 @@ import keras.utils as utils
 from preprocessor import load_dataset
 import numpy as np
 import os.path
+import random
+import matplotlib.pyplot as plt
 
 #For F1 score
 from keras import backend as K
 
 #Tensorboard imports
-from time import time
+import time
 from tensorflow.python.keras.callbacks import TensorBoard
 
 #To load and save models
@@ -22,9 +24,7 @@ from keras.models import load_model
 from tensorflow.python.keras.callbacks import ModelCheckpoint
 
 #Model path
-model_name = "model.h5"
 model_dir = "saved_models"
-model_path = model_dir + "/" + model_name
 log_dir = "logs"
 
 
@@ -43,10 +43,18 @@ def main():
     if not os.path.exists(log_dir):
         os.makedirs(log_dir)
         
+    
+    model_name = "model" + "-".join(str(x) for x in options.layers) + " " + str(options.dropout_prob)
+    model_path = model_dir + "/" + model_name + ".h5"
+        
         
     #Collect the dataset. Input is boolean for whether to debug which we need to be
     #set to false.
     (X, Y) = load_dataset(False)
+       
+    plt.hist(Y, bins = [0, 1, 2, 3, 4]) 
+    plt.title("histogram") 
+    plt.show()
     
     if options.debug:
         print("Orignial X shape: ", X.shape)
@@ -96,10 +104,12 @@ def main():
                       metrics=["accuracy", f1])
     else:
         model = load_model(model_path, custom_objects={'f1': f1})
+        if options.debug:
+            print("model loaded sucessfully")
     
     #To run tensorboard, run this line in your terminal:
     #tensorboard --logdir=logs/
-    tensor_board = TensorBoard(log_dir = log_dir+"/{}".format(time()))
+    tensor_board = TensorBoard(log_dir=log_dir+"/{0}_{1}".format(time.asctime(time.localtime(time.time())).replace(":", "-"), model_name))
     
     #Will save the model every epoch if it is the best model so far in terms of validation accuracy.
     model_checkpoint = ModelCheckpoint(model_path, monitor='val_acc', save_best_only=True, mode='auto', period=1)
@@ -113,11 +123,16 @@ def main():
     if options.debug:
         print("")
         print(model.summary())
-        drug_index = 23
-        print("Actual Y test: ", np.argmax(y_test[drug_index]))
-        prediction = model.predict(x_test)[drug_index]
-        print("Predicted max Y test: ", np.argmax(prediction))
-        print("Predicted Y test output: ", prediction)
+        data_point = random.randint(1, 4300)
+        (X_subject, Y_subject) = load_dataset(False, data_point)
+        print("Looking at person on row: ", data_point + 1)
+        prediction = model.predict(X_subject)
+        for drug_id in range(len(X_subject)):
+            print("Drug id: ", X_subject[drug_id, 1])
+            print("Actual Y: ", Y_subject[drug_id])
+            print("Predicted max Y: ", np.argmax(prediction[drug_id]))
+            print("Predicted Y output: ", prediction[drug_id])
+            print("")
 
 
 
