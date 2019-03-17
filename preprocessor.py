42df2f8dddf1f0570b3ec7da8e2b508fab8be8f2
Cory Edwards <14corman@gmail.com>
2019-03-17 12:00:23 -0500

    Created formatted dataset file for tests
    I made the preprocess file now be able to export a formatted excel sheet for analysis.
    I then made  the excel sheet and am providing it.

--- preprocessor.py
+++ preprocessor.py
@@ -8,8 +8,7 @@ import csv
 import xlrd     #If you need to install, use "pip install xlrd"
 import os
 import numpy as np
-import math
-import random
+import xlsxwriter
 
 # All start and end positions for required variables in the csv.
 geneStart = 46
@@ -17,7 +16,23 @@ geneEnd = 197
 
 micStart = 19   # 0 indexing
 micEnd = 45
-#emptyMics = {20, 24, 28, 31, 35, 41}
+
+ageIndex = 200
+sexIndex = 201
+siteIndex = 2
+stateIndex = 6
+
+states = ['AK', 'AL', 'AR', 'AZ', 'CA', 'CO', 'DE', 'Dgo', 'FL', 'GA', 'IA', 'IL', 'IN', 'Jaliso', 'KS', 'KY', 'LA', 
+          'MA', 'MD', 'ME', 'MI', 'MN', 'MO', 'NC', 'ND', 'NE', 'New South Wales', 'NJ', 'NM', 'NY', 'OH', 'OR', 'PA', 
+          'QLD', 'TN', 'TX', 'UT', 'VA', 'VIC', 'VT', 'WA', 'Western Australia', 'WI' ]
+
+siteCodes = [2, 3, 4, 12, 13, 15, 17, 21, 24, 25, 27, 30, 39, 40, 43, 46, 48, 49, 51, 52, 57, 62, 63, 64, 65, 66, 68, 69, 75, 81, 
+             86, 89, 90, 91, 96, 97, 100, 101, 102, 106, 107, 112, 113, 115, 116, 117, 120, 122, 126, 127, 129, 130, 131, 133, 134, 
+             136, 137, 138, 139, 146, 148, 149, 150, 203, 215, 219, 229, 260, 262, 263, 277, 279, 280, 281, 283, 302, 303, 307, 317,
+             329, 333, 336, 342, 346, 347, 349, 361, 364, 365, 370, 371, 376, 377, 379, 380, 404, 410, 411, 412, 413, 417, 420, 422, 
+             425, 426, 433, 437, 442, 443, 448, 452, 453, 454, 455, 456, 457, 460, 461, 462, 464, 467, 468, 469, 470, 471, 472, 473, 
+             475, 476, 477, 480, 481, 601, 603, 605, 606, 614, 616, 703, 704, 721, 726, 728, 732, 734, 739, 742, 760, 789, 792, 794, 
+             800, 806, 810, 812, 814, 815 ]
 
 #The path to Required Information for Analysis.xlsx
 reqInformationPath = os.getcwd() + "/Required Information for Analysis.xlsx"
@@ -27,7 +42,7 @@ debug = False
 
 
 
-def load_dataset(d, rowNum=None, predict=False):
+def load_dataset(d, rowNum=None, predict=False, export=False):
     """
     Author Cory Kromer-Edwards
     Edits by: Andrew West
@@ -39,58 +54,96 @@ def load_dataset(d, rowNum=None, predict=False):
     Y = []
     
     with open('dataset.csv') as csvFile:
-        csvReader = csv.reader(csvFile, delimiter=',')
-        lineCount = 0
-        (esblNames, carbaNames, micBreakpoints, betaDrugs) = getRequiredInfo()
-        for row in csvReader:
-            if lineCount == 0:
-                (isolateNames, drugNames) = getNames(row)
-                if debug:
-                    print(f'Column names are:\n{", ".join(row)}')
-                lineCount += 1
-            elif lineCount > 5 and debug:
-                break
-            else:
-                if testRow(row):
-                    
-                    if rowNum and rowNum == lineCount:
-                        X = []
-                        Y = []
-                  
-                    #Get all data for row.
-                    isolate = getGenes(row)
-                    (convertedEsblCarba, numCategory) = convertEsblCarba(isolateNames, esblNames, carbaNames, isolate)
-                    (mics, actualMics) = getMicValues(row, micBreakpoints, betaDrugs, drugNames)
-                    
-                    #Append X and Y for dataset
-                    for i in range(len(mics)):
-                        if ((mics[i][0] == 0 and random.random() < 0.1) \
-                          or (mics[i][0] == 1 and random.random() < 0.15) \
-                          or mics[i][0] == 2 \
-                          or (mics[i][0] == 3 and random.random() < 0.15)) or predict:
-                            X.append([convertedEsblCarba, mics[i][1], numCategory]) #, actualMics[i]])
-                            Y.append(mics[i][0])
-                    
-                    
-                    if debug or (rowNum and rowNum == lineCount):
-                        print("\nRow number " + str(lineCount))
-                        print("Genes: ", isolate)
-                        print("ESBL, Carba: ", convertedEsblCarba, " - ", ["ESBL", "Carba.", "both", "neither"][convertedEsblCarba])
-                        print("Num genes ESBL or Carba: ", str(numCategory))
-                        print("MICs: ", mics, " - ", [["NA", "R", "I", "S"][mic] + "/" + str(drugId) for mic, drugId in mics])
-                        print("Actual MIC values: [", [str(actualMic) + "," for actualMic in actualMics], "]")
+        with xlsxwriter.Workbook('formated data.xlsx') as workbook:
+            formatted = workbook.add_worksheet()
+            csvReader = csv.reader(csvFile, delimiter=',')
+            lineCount = 0
+            formatedLines = 1
+            (esblNames, carbaNames, micBreakpoints, betaDrugs) = getRequiredInfo()
+            for row in csvReader:
+                if lineCount == 0:
+                    (isolateNames, drugNames) = getNames(row)
+                    formatted.write(0, 0, "Site")
+                    formatted.write(0, 1, "State")
+                    formatted.write(0, 2, "Gender")
+                    formatted.write(0, 3, "Age")
+                    formatted.write(0, 4, "MIC num undefined")
+                    formatted.write(0, 5, "MIC num R")
+                    formatted.write(0, 6, "MIC num I")
+                    formatted.write(0, 7, "MIC num S")
+                    if debug:
+                        print(f'Column names are:\n{", ".join(row)}')
+                    lineCount += 1
+                elif lineCount > 5 and debug:
+                    break
+                else:
+                    if testRow(row):
                         
-                    if rowNum and rowNum == lineCount:
-                        return (np.asarray(X, dtype=np.float32), np.asarray(Y, dtype=np.float32))
+                        if rowNum and rowNum == lineCount:
+                            X = []
+                            Y = []
+                      
+                        #Get all data for row.
+                        isolate = getGenes(row)
+                        (convertedEsblCarba, numCategory) = convertEsblCarba(isolateNames, esblNames, carbaNames, isolate)
+                        (mics, actualMics) = getMicValues(row, micBreakpoints, betaDrugs, drugNames)
                         
-                    
-                lineCount += 1
+                        if debug or (rowNum and rowNum == lineCount):
+                            print("\nRow number " + str(lineCount))
+                            print("Genes: ", isolate)
+                            print("ESBL, Carba: ", convertedEsblCarba, " - ", ["ESBL", "Carba.", "both", "neither"][convertedEsblCarba])
+                            print("Num genes ESBL or Carba: ", str(numCategory))
+                            print("MICs: ", mics, " - ", [["NA", "R", "I", "S"][mic] + "/" + str(drugId) for mic, drugId in mics])
+                            print("Actual MIC values: [", [str(actualMic) + "," for actualMic in actualMics], "]")
+                        
+                        if export:
+                            age = getAgeBin(row)
+                            sex = 0 if row[sexIndex] == "M" else 1
+                            site = siteCodes.index(int(row[siteIndex]))
+                            state = states.index(row[stateIndex]) if row[stateIndex] != "" else -1
+                            
+                            formatted.write(formatedLines, 0, site)
+                            formatted.write(formatedLines, 1, state)
+                            formatted.write(formatedLines, 2, sex)
+                            formatted.write(formatedLines, 3, age)
+                            undef = 0
+                            R = 0
+                            I = 0
+                            S = 0
+                            for i in range(len(mics)):
+                                if mics[i][0] == 0:
+                                    undef += 1
+                                elif mics[i][0] == 1:
+                                    R += 1
+                                elif mics[i][0] == 2:
+                                    I += 1
+                                elif mics[i][0] == 3:
+                                    S += 1
+                              
+                            formatted.write(formatedLines, 4, undef)
+                            formatted.write(formatedLines, 5, R)
+                            formatted.write(formatedLines, 6, I)
+                            formatted.write(formatedLines, 7, S)
+                                
+                            formatedLines += 1
+                          
+                        else:
+                            #Append X and Y for dataset
+                            for i in range(len(mics)):
+                                X.append([convertedEsblCarba, mics[i][1], numCategory])
+                                Y.append(mics[i][0])
+                                
+                            if rowNum and rowNum == lineCount:
+                                return (np.asarray(X, dtype=np.float32), np.asarray(Y, dtype=np.float32))
+                            
+                        
+                    lineCount += 1
                 
         print(f'Processed {lineCount} lines.')
     return (np.asarray(X, dtype=np.float32), np.asarray(Y, dtype=np.float32))
 
 def main():
-    load_dataset(True)
+    load_dataset(False, export=True)
     
         
 def testRow(row):
@@ -106,6 +159,15 @@ def testRow(row):
     for i in range(geneStart, geneEnd):
         if row[i] == "":
             return False
+          
+    if row[ageIndex] == "":
+        return False
+      
+    if row[sexIndex] == "":
+        return False
+      
+    if row[siteIndex] == "":
+        return False
 
     return True
   
@@ -126,6 +188,60 @@ def getNames(row):
         
     return (isolate, drugNames)
   
+def getAgeBin(row):
+    """
+    Author Cory Kromer-Edwards
+    Edits by: ...
+    Takes the age of a subject and places them in the correct bin.
+    """
+    age = int(row[ageIndex])
+    if debug:
+        print("Age: ", age)
+        
+    #Pick 1 of 20 bins
+    if age >= 0 and age < 5:
+      return 0
+    elif  age >= 5 and age < 10:
+      return 1
+    elif  age >= 10 and age < 15:
+      return 2
+    elif  age >= 15 and age < 20:
+      return 3
+    elif  age >= 20 and age < 25:
+      return 4
+    elif  age >= 25 and age < 30:
+      return 5
+    elif  age >= 30 and age < 35:
+      return 6
+    elif  age >= 35 and age < 40:
+      return 7
+    elif  age >= 40 and age < 45:
+      return 8
+    elif  age >= 45 and age < 50:
+      return 9
+    elif  age >= 50 and age < 55:
+      return 10
+    elif  age >= 55 and age < 60:
+      return 11
+    elif  age >= 60 and age < 65:
+      return 12
+    elif  age >= 65 and age < 70:
+      return 13
+    elif  age >= 70 and age < 75:
+      return 14
+    elif  age >= 75 and age < 80:
+      return 15
+    elif  age >= 80 and age < 85:
+      return 16
+    elif  age >= 85 and age < 90:
+      return 17
+    elif  age >= 90 and age < 95:
+      return 18
+    elif  age >= 95 and age < 100:
+      return 19
+    else:
+      return -1
+  
 def getRequiredInfo():
     """
     Author: Cory Kromer-Edwards
@@ -141,23 +257,23 @@ def getRequiredInfo():
     betaDrugs = set()
     mic = dict()
     
-    wb = xlrd.open_workbook((reqInformationPath)) 
-    sheet_0 = wb.sheet_by_index(0)
-    sheet_2 = wb.sheet_by_index(2)
-    sheet_3 = wb.sheet_by_index(3)
-
-    #range is (row start, row end) of the excel sheet, and it starts at 0.
-    for i in range(1, 20):
-        betaDrugs.add(sheet_0.cell_value(i, 1))
-        
-    for i in range(1, 48):
-        mic[sheet_3.cell_value(i, 0)] = (sheet_3.cell_value(i, 1), sheet_3.cell_value(i, 2))
-      
-    for i in range(1, 424): 
-        esbl.add(sheet_2.cell_value(i, 0))
-                
-    for i in range(1, 585): 
-        carba.add(sheet_2.cell_value(i, 1))
+    with xlrd.open_workbook((reqInformationPath)) as wb:
+        sheet_0 = wb.sheet_by_index(0)
+        sheet_2 = wb.sheet_by_index(2)
+        sheet_3 = wb.sheet_by_index(3)
+    
+        #range is (row start, row end) of the excel sheet, and it starts at 0.
+        for i in range(1, 20):
+            betaDrugs.add(sheet_0.cell_value(i, 1))
+            
+        for i in range(1, 48):
+            mic[sheet_3.cell_value(i, 0)] = (sheet_3.cell_value(i, 1), sheet_3.cell_value(i, 2))
+          
+        for i in range(1, 424): 
+            esbl.add(sheet_2.cell_value(i, 0))
+                    
+        for i in range(1, 585): 
+            carba.add(sheet_2.cell_value(i, 1))
                 
     return (esbl, carba, mic, betaDrugs)
   
@@ -187,10 +303,7 @@ def convertEsblCarba(isolateNames, esbl, carba, isolate):
                 carbaBool = True
                 if debug:
                     print(isolateNames[i])
-                
-#        if esblBool and carbaBool:
-#            break
-    
+                    
     if not esblBool and not carbaBool:
         return (3, numGenesInGroup)  #Neither
     elif esblBool and carbaBool:
